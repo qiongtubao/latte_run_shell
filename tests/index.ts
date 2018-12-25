1322715a139b7ba958e4ff6f5f432a523bfa9721
qiongtubao <810776886@qq.com>
2018-12-26 01:26:20 +0800

    update

--- tests/index.ts
+++ tests/index.ts
@@ -2,8 +2,6 @@ import * as latte_run_shell from "../src/index"
 let shell = latte_run_shell.create({
 
 })
-shell.exec('echo $http_proxy', function(err, data) {
+shell.exec('echo $http_proxy && echo $https_proxy', function(err, data) {
   console.log(err, data)
-}).exec("cd /", function(err, data) {
-    console.log('echo', err, data)
 })
--- tsd/index.d.ts
+++ tsd/index.d.ts
@@ -2,11 +2,13 @@
 import { ChildProcess } from "child_process";
 declare class Shell {
     process: ChildProcess;
+    onData: Function;
+    caches: any[];
     constructor();
     error(error: any): void;
     data(data: any): void;
     export(key: any, value: any): void;
-    exec(cmd: any, callback?: any): void;
+    exec(cmd: any, callback?: any): this;
 }
 export declare function create(options: any): Shell;
 export {};
